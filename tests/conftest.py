ed4bce621d7daca9213c3eb1106c2b1b9aa83e29
Aleksandra Kashina <Borland3711@gmail.com>
2023-09-08 13:10:39 +0300

    moved folder 'utils'

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 
-from utils import attach
+from tests.utils import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
